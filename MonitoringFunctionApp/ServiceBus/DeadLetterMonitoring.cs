dac9223f9a6e544540d036f982090fad1174bece
Georgia Manchershaw <georgia.manchershaw@asos.com>
2018-05-16 18:21:24 +0100

    adding custom event for all subscriptions in topic

--- MonitoringFunctionApp/ServiceBus/DeadLetterMonitoring.cs
+++ MonitoringFunctionApp/ServiceBus/DeadLetterMonitoring.cs
@@ -1,36 +1,56 @@
-﻿using System.Configuration;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.ServiceBus;
 
 namespace MonitoringFunctionApp.ServiceBus
 {
     public static class DeadLetterMonitoring
     {
-        private static readonly string ConnectionString = ConfigurationManager.AppSettings["ConnString"];
-        private static readonly string InstrumentationKey = ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+        private static readonly string ServiceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
+        private static readonly string InstrumentationKey = "adf0beae-a23e-45a3-b5c1-2ad9cbedfca4";
         private static NamespaceManager _namespaceManager;
 
-        [FunctionName("DeadLetterCount")]
-        public static async Task MonitorDeadLetterCount([TimerTrigger("0 */5 * * * *")]TimerInfo timer, TraceWriter log)
+        [FunctionName("dead-letter-count")]
+        public static async Task MonitorDeadLetterCount([TimerTrigger("0 */5 * * * *")]TimerInfo timer)
         {
-            _namespaceManager = NamespaceManager.CreateFromConnectionString(ConnectionString);
-
-            var subscription = await _namespaceManager.GetSubscriptionAsync("topicPath", "topicName");
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(ServiceBusConnectionString);
 
             var deadLetterMessageCount = 0L;
 
-            deadLetterMessageCount = subscription.MessageCountDetails.DeadLetterMessageCount;
-           
+            var topics = await _namespaceManager.GetTopicsAsync();
+
             var telemetryClient = new TelemetryClient(new TelemetryConfiguration(InstrumentationKey));
 
-            var deadLetterMetric = new MetricTelemetry("DeadLetterCount", deadLetterMessageCount);
+            var customEvent = new EventTelemetry("Subscription Dead Letter Count");
+
+            foreach (var topic in topics)
+            {
+                if (topic.Path.Contains("warehouse-orders"))
+                {
+                    var subscriptions = await _namespaceManager.GetSubscriptionsAsync(topic.Path);
+                    
+                    foreach (var subscription in subscriptions)
+                    {
+                        deadLetterMessageCount = subscription.MessageCountDetails.DeadLetterMessageCount;
+                        
+                        customEvent.Metrics.Add(subscription.Name, deadLetterMessageCount);
+
+                    }
+                }
+            }
+
+            telemetryClient.TrackEvent(customEvent);
             
-            telemetryClient.TrackMetric(deadLetterMetric);
+
+            //var telemetry  = new TraceTelemetry(subscription.Name);
+            //telemetry.Properties.Add("Dead Letter Count", deadLetterMessageCount.ToString());
+
+           //var deadLetterMetric = new MetricTelemetry("Dead Letter Count", deadLetterMessageCount);
+           //telemetryClient.TrackMetric(deadLetterMetric);
         }
     }
 }
